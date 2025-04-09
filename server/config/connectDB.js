40c75b8d68440067c9f2a8c225f92fead41776ac
Aayush Ghimire <ayushghimire077@gmail.com>
2025-04-09 10:25:12 +0545

    Changed in connectdb

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -1,10 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+
+let isConnected = false;  
 
 export const connectDB = async () => {
-    try {
-        const connection = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected`);
-    } catch (error) {
-        console.log(error)
-    }
-}
+  if (isConnected) return; 
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection failed:", err);
+  }
+};
